94e0f17a7481976e6118a429177e166cbd1b33c3
Alexandre Gabriele <alexandre.gabriele@orange.fr>
2021-06-30 00:49:30 +0200

    version soutenance

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,7 +1,21 @@
 <template>
+<div>
   <div class="about">
     <h1>Cher Collaborateur, Nous te souhaitons la Bienvenue !!!</h1>
   </div>
+
+  <div class="contenair">
+    <div class="card">
+      <div class="card-title">
+        <h2> Attention Rappels!!!</h2>
+      </div>
+      <div class="card-body">
+        <p>Bienvenue dans votre espace de détente, nous vous rappelons que ce forum est modéré, ainsi nous vous demandons du respect et de la bienveillance entre vous !</p>
+        <p>Amusez vous bien !</p>
+      </div>
+    </div>
+  </div>
+ </div>   
 </template>
 
 <style lang="scss" scoped>
